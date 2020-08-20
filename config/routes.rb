c9c04832a8e531783dd435d30ee234617792b230
Franz <Franz@LAPTOP-JC7PJSNR>
2020-08-20 15:02:14 +0800

    commit alpha

--- config/routes.rb
+++ config/routes.rb
@@ -9,3 +9,4 @@ Rails.application.routes.draw do
   delete 'articles/:id' => 'articles#delete', as: 'delete_article'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
